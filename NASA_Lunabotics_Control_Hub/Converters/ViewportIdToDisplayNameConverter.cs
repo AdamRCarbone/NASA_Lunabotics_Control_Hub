bef71142b2dd172ee33cf59f989bf4f40ad66557
Adam Carbone <adam.r.carbone@live.com>
2026-04-18 23:07:21 -0400

    feat: fix viewport labeling and switching
    - Changed 'Waiting for /slam/map' to 'Waiting for Live 3D Map' as requested
    - Added viewport header above center showing active viewport name (Live 3D Map Feed, Depth Camera Feed, etc.)
    - Simplified center viewport to show active feed name with appropriate icon
    - All camera cards and 3D Map card switch viewport when clicked
    - Visual feedback: active items show CSU lime green border (#C5D86D)
    - Removed unused visibility converter
    - Maintained all existing functionality: mode switching, E-Suggestion, expanding telemetry sections

---
+++ NASA_Lunabotics_Control_Hub/Converters/ViewportIdToDisplayNameConverter.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace NASA_Lunabotics_Control_Hub.Converters;
+
+public class ViewportIdToDisplayNameConverter : IValueConverter
+{
+    public static readonly ViewportIdToDisplayNameConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string viewportId)
+        {
+            return viewportId switch
+            {
+                "map" => "Live 3D Map",
+                "depth_cam" => "Depth Camera",
+                "left_side" => "Left Side Camera",
+                "left_front" => "Left Front Camera",
+                "right_side" => "Right Side Camera",
+                "right_front" => "Right Front Camera",
+                "back_rear" => "Back Rear Camera",
+                "far_front" => "Far Front Camera",
+                "far_right" => "Far Right Camera",
+                "far_back" => "Far Back Camera",
+                "far_left" => "Far Left Camera",
+                _ => "Viewport"
+            };
+        }
+        return "Viewport";
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
